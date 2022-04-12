1d23a96810fabd41bc1f0152ad148eee4627cbfe
eodero <erickarthurs13@gmail.com>
2022-04-11 23:41:28 -0500

    Used e.target from previous task, added a fieldset to the forms and also created an edit button. Finally styled the web page using the created css.index file

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const today = new Date();
 const thisYear = today.getFullYear()
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-copyright.innerHTML = `<span>Erick Odero ${thisYear}</span> `;
+copyright.innerHTML = `<span> &copy; Erick Odero ${thisYear}</span> `;
 footer.appendChild(copyright);
 
 let skills = ['html', 'CSS', 'JavaScript'];
@@ -15,37 +15,48 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
-// Handle message from submit
+// Handle message form submit and edit button
 const messageForm = document.querySelector('form[name="leave_message"]')
+const editButton = document.createElement('button');
+editButton.classList.add('edit');
+editButton.innerText = 'Edit';
+editButton.type = 'button';
+messageForm.appendChild(editButton);
+
 messageForm.addEventListener('submit', (e) => {
     // prevent default refreshing behavior
     e.preventDefault();
 
     // create variables for each form field
-    const user_name = messageForm.name.value;
-    const user_email = messageForm.email.value;
-    const user_message = messageForm.message.value;
+    // const user_name = messageForm.name.value;
+    // const user_email = messageForm.email.value;
+    // const user_message = messageForm.message.value;
+
+    const user_name = e.target.name;
+    const user_email = e.target.email;
+    const user_message = e.target.message;
 
     // Display messages in list
     const messageSection = document.getElementById('messages');
     const messageList = messageSection.children[1];
     const newMessage = document.createElement('li');
-    newMessage.innerHTML = `<a href="mailto:${user_email}">${user_name}</a><span> wrote: ${user_message}</span>`;
-    messageList.appendChild(newMessage);
+    newMessage.innerHTML = `<a href="mailto:${user_email.value}">${user_name.value}</a><span> wrote: ${user_message.value}</span>`;
+
 
     // create button element
     const removeButton = document.createElement('button');
     removeButton.innerText = 'remove';
     removeButton.type = 'button';
-    newMessage.appendChild(removeButton);
+
 
     // handle the removeButton
     removeButton.addEventListener('click', (e) => {
-        const entry = removeButton.parentNode;
+        const entry = e.target.parentNode;
         entry.remove();
-
     })
     // reset the form
     messageForm.reset();
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
 
 })
