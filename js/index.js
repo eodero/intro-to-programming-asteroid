c9e7e9c7ca4443a1585035838d8df77b37a4e5b3
eodero <erickarthurs13@gmail.com>
2022-05-19 13:52:10 -0500

    Added images folder with LinkedIn and GitHub icons. Added LinkedIn and Github icons to the footer with associated links to respective pages. Edited CSS file to reposition footer to span entire page width. In the process of adding edit button functionality and hide empty message section

--- js/index.js
+++ js/index.js
@@ -2,12 +2,16 @@ const today = new Date();
 const thisYear = today.getFullYear()
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
+copyright.classList.add('copyright');
 copyright.innerHTML = `<span> &copy; Erick Odero ${thisYear}</span> `;
 footer.appendChild(copyright);
+let repositories;
 
-let skills = ['html', 'CSS', 'JavaScript'];
-const skillsSection = document.getElementById('skills');
-const skillsList = document.getElementById('skills').children[1];
+
+// Added skills and edited section
+let skills = ['HTML', 'CSS', 'JavaScript', 'Ajax', 'Fetch-API'];
+// const skillsSection = document.getElementById('skills');
+const skillsList = document.getElementById('skillz');
 
 for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement('li');
@@ -15,22 +19,25 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
-// Handle message form submit and edit button
-const messageForm = document.querySelector('form[name="leave_message"]')
+// Create and handle edit button
+const editMessage = document.getElementById('text_message');
 const editButton = document.createElement('button');
-editButton.classList.add('edit');
-editButton.innerText = 'Edit';
+editButton.textContent = 'Edit';
 editButton.type = 'button';
-messageForm.appendChild(editButton);
+editButton.classList.add('btn');
+editMessage.appendChild(editButton);
+
+editButton.addEventListener('click', (e) => {
+    const messageText = document.getElementById('textMessage');
+    messageText.textContent = text;
+})
+
+// Handle message form submit  button
+const messageForm = document.querySelector('form[name="leave_message"]')
 
 messageForm.addEventListener('submit', (e) => {
     // prevent default refreshing behavior
     e.preventDefault();
-
-    // create variables for each form field
-    // const user_name = messageForm.name.value;
-    // const user_email = messageForm.email.value;
-    // const user_message = messageForm.message.value;
 
     const user_name = e.target.name;
     const user_email = e.target.email;
@@ -43,11 +50,10 @@ messageForm.addEventListener('submit', (e) => {
     newMessage.innerHTML = `<a href="mailto:${user_email.value}">${user_name.value}</a><span> wrote: ${user_message.value}</span>`;
 
 
-    // create button element
+    // create remove button element
     const removeButton = document.createElement('button');
     removeButton.innerText = 'remove';
     removeButton.type = 'button';
-
 
     // handle the removeButton
     removeButton.addEventListener('click', (e) => {
@@ -58,5 +64,38 @@ messageForm.addEventListener('submit', (e) => {
     messageForm.reset();
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
-
 })
+//hide message section
+
+const msgList = document.getElementsByClassName('msgList');
+for (let i = 0; i < msgList.length; i++) {
+    let li = msgList[i];
+    if (!li.getElementsByTagName('li').length) {
+        li.style.display = 'none';
+    } else {
+        li.style.display = 'block';
+    }
+}
+
+const header = document.getElementById('header');
+header.classList.add('sticky');
+
+fetch('https://api.github.com/users/eodero/repos')
+    .then(response => response.json())
+    .then(data => displayRepo(data))
+    .catch(error => console.log('Error detected!', error))
+
+function displayRepo(data) {
+    const projectSection = document.getElementById('projects');
+    const projectList = projectSection.lastElementChild;
+    for (let i = 0; i < data.length; i++) {
+        const project = document.createElement('li');
+
+        project.innerText = data[i].name;
+        // console.log(project.innerText = repositories[i].name)
+        projectList.appendChild(project);
+    }
+}
+
+
+
